312314d54be46803c3b42b4326e7c5ce8e394384
cornernote <cornernote@gmail.com>
2016-04-29 11:46:11 +0930

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,93 @@
-# yii-asset-compress
-Command to merge and compress assets for Yii
+# Yii Asset Compress
+
+Command to merge and minify assets for Yii.
+
+## Features
+
+- Merges and minifies lists of CSS or JS files into a single CSS or JS files.
+- Replaces releative `url()` in CSS files
+- Publishes required assets so that relative assets are available
+
+## Documentation
+
+Add to your CLI config:
+
+```
+$config = array(
+    'commandMap' => array(
+        'assetCompress' => array(
+            'class' => 'vendor.cornernote.yii-asset-compress.commands.AssetCompressCommand',
+            'assetsPath' => 'application.assets',
+            'css' => array(
+                'combine' => array(
+                    'css/desktop.css' => array(
+                        // format is: asset.path.alias|path/to/asset.css
+                        'vendor.twbs.bootstrap.dist|css/bootstrap.css',             // -{ (alias!=application) = this asset path will be
+                        'bootstrap.assets|css/yiistrap.css',                        // -{ published, and any url() in the CSS will be 
+                        'vendor.fortawesome.font-awesome|css/font-awesome.min.css', // -{ replaced with this path.
+                        'application.assets|css/desktop.css',                       // -{
+                        'application|css/desktop.css',                              //  - (alias=application) = webroot, assets not published
+                    ),
+                    'css/mobile.css' => array(
+                        'vendor.twbs.bootstrap.dist|css/bootstrap.css',
+                        'bootstrap.assets|css/yiistrap.css',
+                        'vendor.fortawesome.font-awesome|css/font-awesome.min.css',
+                        'application.assets|css/mobile.css',
+                        'application|css/mobile.css',
+                    ),
+                ),
+                'minify' => true
+            ),
+            'js' => array(
+                'combine' => array(
+                    'js/desktop.js' => array(
+                        // format is: asset.path.alias|path/to/asset.js
+                        'system.web.js.source|jquery.min.js',            // -{ (alias!=application) = this asset path will be
+                        'system.web.js.source|jquery.yiiactiveform.js',  // -{ published, and any url() in the CSS will be 
+                        'vendor.twbs.bootstrap.dist|js/bootstrap.js',    // -{ replaced with this path.
+                        'application.assets|js/desktop.js',              // -{ 
+                        'application|js/desktop.js',                     // - webroot, assets not published
+                    ),
+                    'js/mobile.js' => array(
+                        'system.web.js.source|jquery.min.js',
+                        'system.web.js.source|jquery.yiiactiveform.js',
+                        'vendor.twbs.bootstrap.dist|js/bootstrap.js',
+                        'application.assets|js/mobile.js',
+                        'application|js/mobile.js',
+                    ),
+                ),
+                'minify' => true
+            )
+        ),
+    ),
+);
+```
+
+Then run from your YIIC command:
+
+```
+php yiic assetCompress
+```
+
+
+## Resources
+
+- **[GitHub Project](https://github.com/cornernote/yii-asset-compress)**
+- **[Yii Extension](http://www.yiiframework.com/extension/yii-asset-compress)**
+
+
+## Support
+
+- Does this README need improvement?  Go ahead and [suggest a change](https://github.com/cornernote/yii-asset-compress/edit/master/README.md).
+- Found a bug, or need help using this project?  Check the [open issues](https://github.com/cornernote/yii-asset-compress/issues) or [create an issue](https://github.com/cornernote/yii-asset-compress/issues/new).
+
+
+## License
+
+[BSD-3-Clause](https://raw.github.com/cornernote/yii-asset-compress/master/LICENSE), Copyright © 2017 [Mr PHP](mailto:info@mrphp.com.au)
+
+
+[![Mr PHP](https://raw.github.com/cornernote/mrphp-assets/master/img/code-banner.png)](http://mrphp.com.au)
+
+[![Latest Stable Version](https://poser.pugx.org/cornernote/yii-asset-compress/v/stable.png)](https://github.com/cornernote/yii-asset-compress/releases/latest) [![Total Downloads](https://poser.pugx.org/cornernote/yii-asset-compress/downloads.png)](https://packagist.org/packages/cornernote/yii-asset-compress) [![Monthly Downloads](https://poser.pugx.org/cornernote/yii-asset-compress/d/monthly.png)](https://packagist.org/packages/cornernote/yii-asset-compress) [![Latest Unstable Version](https://poser.pugx.org/cornernote/yii-asset-compress/v/unstable.png)](https://github.com/cornernote/yii-asset-compress) [![Build Status](https://travis-ci.org/cornernote/yii-asset-compress.png?branch=master)](https://travis-ci.org/cornernote/yii-asset-compress) [![License](https://poser.pugx.org/cornernote/yii-asset-compress/license.png)](https://raw.github.com/cornernote/yii-asset-compress/master/LICENSE)
+
---
+++ commands/AssetCompressCommand.php
@@ -0,0 +1,189 @@
+<?php
+
+/**
+ * Class AssetCompressCommand
+ * @see https://github.com/aarondfrancis/mantis-manager
+ *
+ * @usage:
+ * ```
+ * 'commandMap' => array(
+ *   'assetCompress' => array(
+ *     'class' => 'vendor.cornernote.yii-asset-compress.commands.AssetCompressCommand',
+ *     'assetsPath' => 'application.assets',
+ *     'css' => array(
+ *       'combine' => array(
+ *         'css/combined.css' => array(
+ *           'vendor.twbs.bootstrap.dist|css/bootstrap.css',
+ *           'bootstrap.assets|css/yiistrap.css',
+ *           'vendor.fortawesome.font-awesome|css/font-awesome.min.css',
+ *           'application.assets|css/app.css',
+ *           'application|css/app.css',
+ *         ),
+ *       ),
+ *       'minify' => true
+ *     ),
+ *     'js' => array(
+ *       'combine' => array(
+ *         'js/combined.js' => array(
+ *           'system.web.js.source|jquery.min.js',
+ *           'system.web.js.source|jquery.yiiactiveform.js',
+ *           'vendor.twbs.bootstrap.dist|js/bootstrap.js',
+ *           'application.assets|js/app.js',
+ *           'application|js/app.js',
+ *         ),
+ *       ),
+ *       'minify' => true
+ *     ),
+ *   ),
+ * ),
+ * ```
+ */
+class AssetCompressCommand extends CConsoleCommand
+{
+
+    /**
+     * @var string
+     */
+    public $assetsPath = 'application.assets';
+    /**
+     * @var array
+     */
+    public $css = array();
+    /**
+     * @var array
+     */
+    public $js = array();
+
+    /**
+     * @var
+     */
+    private $_assetsPath;
+
+    /**
+     *
+     */
+    public function init()
+    {
+        $this->_assetsPath = Yii::getPathOfAlias($this->assetsPath);
+
+        $this->css = CMap::mergeArray(array(
+            'combine' => array(),
+            'minify' => true
+        ), $this->css);
+
+        $this->js = CMap::mergeArray(array(
+            'combine' => array(),
+            'minify' => true
+        ), $this->js);
+    }
+
+    /**
+     *
+     */
+    public function actionIndex()
+    {
+        $this->consoleEcho("Asset Compress\r\n", "0;35");
+        $combine = array_merge($this->css['combine'], $this->js['combine']);
+        foreach ($combine as $filename => $files) {
+            $this->consoleEcho("Combining ", "0;32");
+            $this->consoleEcho("for " . $this->_assetsPath . '/' . $filename . "\r\n");
+            $content = $this->combine($files);
+
+            $ext = pathinfo($filename, PATHINFO_EXTENSION);
+
+            if ($ext == "css" && $this->css['minify']) {
+                $this->consoleEcho("Minifying ", "0;32");
+                $this->consoleEcho(" $filename \r\n");
+                $content = $this->minifyCSS($content);
+            }
+            if ($ext == "js" && $this->js['minify']) {
+                $this->consoleEcho("Minifying ", "0;32");
+                $this->consoleEcho(" $filename \r\n");
+                $content = $this->minifyJS($content);
+            }
+
+            $dir = dirname($this->_assetsPath . '/' . $filename);
+            if (!is_dir($dir)) {
+                mkdir($dir, 0755, true);
+            }
+            $file = new SplFileInfo($this->_assetsPath . '/' . $filename);
+            $this->consoleEcho("Writing ", "0;32", true);
+            $this->consoleEcho($file->getFilename() . " \r\n", null, true);
+            file_put_contents($this->_assetsPath . '/' . $filename, $content);
+        }
+    }
+
+    /**
+     * @param array $files
+     * @return string
+     * @throws CException
+     */
+    private function combine($files)
+    {
+        $content = "";
+        foreach ($files as $file) {
+            $this->consoleEcho("Adding ", "0;32");
+            $this->consoleEcho($file . "\r\n");
+
+            list($_assetPath, $file) = explode('|', $file);
+            if ($_assetPath == 'application') {
+                $assetPath = Yii::getPathOfAlias('www');
+                $assetUrl = Yii::app()->baseUrl;
+            } else {
+                $assetPath = Yii::getPathOfAlias($_assetPath);
+                $assetUrl = Yii::app()->assetManager->publish($assetPath);
+            }
+            $_content = file_get_contents($assetPath . '/' . $file);
+
+            $ext = pathinfo($file, PATHINFO_EXTENSION);
+            if ($ext == 'css') {
+                $assetUrl = ltrim($assetUrl, '.');
+                $relativePath = dirname($assetPath . '/' . $file);
+                $relativeUrl = str_replace($assetPath, $assetUrl, $relativePath);
+                $_content = preg_replace('%url\s*\(\s*[\\\'"]?(?!(((?:https?:)?\/\/)|(?:data:?:)))([^\\\'")]+)[\\\'"]?\s*\)%', 'url("' . $relativeUrl . '/$3")', $_content);
+            }
+            if ($ext == 'js') {
+                $_content .= ';' . "\n";
+            }
+            $content .= $_content;
+        }
+        return $content;
+    }
+
+    /**
+     * @param $contents
+     * @return string
+     */
+    private function minifyCSS($contents)
+    {
+        if (!$contents) return "";
+        return Minify_CSS_Compressor::process($contents);
+    }
+
+    /**
+     * @param $contents
+     * @return string
+     */
+    private function minifyJS($contents)
+    {
+        if (!$contents) return "";
+        return JShrink\Minifier::minify($contents, array('flaggedComments' => false));
+        //return Minify_JS_ClosureCompiler::minify($contents);
+    }
+
+    /**
+     * @param $msg
+     * @param null $color
+     */
+    public function consoleEcho($msg, $color = null)
+    {
+        if (Yii::app() instanceof CConsoleApplication) {
+            if (!is_null($color)) {
+                echo "\033[{$color}m" . $msg . "\033[0m";
+            } else {
+                echo $msg;
+            }
+        }
+    }
+
+}
